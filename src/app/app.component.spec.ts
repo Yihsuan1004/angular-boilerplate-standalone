f68677b2c82bf7b070603f39a2fb0c09837a6538
CieloHung <cielohung@cathayholdings.com.tw>
2024-01-04 16:40:40 +0800

    docs: 新增husky、lint-staged、commitLint、gts(google code style)、調整vscode設定、修改README

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +15,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-boilerplate'`, () => {
+  it('should have as title angular-boilerplate', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('angular-boilerplate');
@@ -24,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-boilerplate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-boilerplate app is running!'
+    );
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {AppRoutingModule} from './app/app-routing.module';
+import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
+}).catch(err => console.error(err));
